77d26f90788ee6a25e7179cb3658a736b50a9bc7
v-statura <valerarl19mgtu@gmail.com>
2024-09-26 19:46:15 +0300

    add to dataBase Home User Role

--- fastapi_application/core/models/modelDb.py
+++ fastapi_application/core/models/modelDb.py
@@ -1,47 +1,72 @@
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
-from sqlalchemy import ForeignKey, Integer, String, DateTime, Text, Boolean, text
-from typing import Annotated
+from sqlalchemy import ForeignKey, Integer, String, DateTime, Text, Boolean, text, Table, Column
+from typing import Annotated, List
 import datetime
 
 
-intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
-created_at = Annotated[datetime.datetime, mapped_column(server_default=text("CURRENT_TIMESTAMP"))]
-updated_at = Annotated[datetime.datetime, mapped_column(
-    server_default=text("CURRENT_TIMESTAMP"),
-    onupdate=text("CURRENT_TIMESTAMP")
-)]
-str_256 = Annotated[str, 256]
-str256 = Annotated[str_256, mapped_column(nullable=False)]
+# intpk = Annotated[int, mapped_column(primary_key=True, autoincrement=True)]
+# created_at = Annotated[datetime.datetime, mapped_column(server_default=text("CURRENT_TIMESTAMP"))]
+# updated_at = Annotated[datetime.datetime, mapped_column(
+    # server_default=text("CURRENT_TIMESTAMP"),
+    # onupdate=text("CURRENT_TIMESTAMP")
+# # )]
+
+# str_256 = Annotated[str, 256]
+# str256 = Annotated[str_256, mapped_column(nullable=False)]
 
 
 class Base(DeclarativeBase):
-    pass
+    created_at = mapped_column(DateTime)
+    updated_at = mapped_column(DateTime)
+
+
+home_users = Table('home_users', Base.metadata, 
+                   Column("user_id", Integer, ForeignKey("user.id")), 
+                   Column("home_id", Integer, ForeignKey("home.id")))
+
 
 class User(Base):
     __tablename__= "user"
 
     __table_args__ = {'extend_existing': True}
 
-    id: Mapped[intpk]
-    hashed_password: Mapped[str256]
-    firstName: Mapped[str256]
-    lastName: Mapped[str256]
-    numberTelephone: Mapped[str256]
-    createdAt: Mapped[created_at]
-    updatedAt:Mapped[updated_at]
-    roleId = mapped_column(ForeignKey('role.id'))
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    hashed_password: Mapped[str] = mapped_column(String(256))
+    first_name: Mapped[str] = mapped_column(String(256)) 
+    last_name: Mapped[str] = mapped_column(String(256))
+    number_telephone: Mapped[str] = mapped_column(String(256))
 
-    role = relationship("Role", uselist=False, back_populates="user")
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('role.id'))
+
+    role: Mapped['Role'] = relationship("Role", uselist=False, back_populates="user")
+    homes: Mapped[List['Home']] = relationship(secondary=home_users)
+
 
 class Role(Base):
     __tablename__ = "role"
 
     __table_args__ = {'extend_existing': True}
 
-    id: Mapped[intpk]
-    roleName: Mapped[str256]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    role_name: Mapped[str] = mapped_column(String(256))
 
-    user = relationship("User", uselist=False, back_populates="role")
+    users: Mapped[List['User']] = relationship("User", back_populates="role")
+
+
+class Home(Base):
+    __tablename__ = "home"
+
+    __table_args__= {'extend_existing': True}
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    home_name: Mapped[str] = mapped_column(String(256))
+    users: Mapped[List['User']] = relationship(secondary=home_users)
+
+
+
+
+
+
 
 
 
