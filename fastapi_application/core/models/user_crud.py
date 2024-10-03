5a19baa9cd5f112fa96eeb776373a35b712463cd
v-statura <valerarl19mgtu@gmail.com>
2024-10-03 10:42:08 +0300

    commit

--- fastapi_application/core/models/user_crud.py
+++ fastapi_application/core/models/user_crud.py
@@ -1,2 +1,27 @@
-import sqlalchemy
-from core.
+from sqlalchemy.orm import Session
+from core.dbHelp import db
+from core.models.modelDb import User
+from core.schemas.user_schemas import UserCreate
+
+
+def get_user_by_id(database: Session, user_id: int):
+    return database.query(User).get(user_id)
+
+
+def delete_user_by_id(database: Session, user_id: int):
+    user = database.query(User).get(user_id)
+    database.delete(user)
+    database.commit()
+    return {'Status': 200}
+
+
+def create_user(database: Session, schema: UserCreate): 
+    user = User(first_name = schema.first_name,
+                last_name = schema.last_name,
+                email = schema.email,
+                hashed_password = schema.hashed_password,
+                number_telephone = schema.number_telephone
+                )
+    database.add(user)
+    database.commit()
+    return {"Status" : 200}
---
+++ fastapi_application/core/schemas/user_schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    hashed_password: str
+    number_telephone: str
+
+
+class UserCreate(UserBase):
+    pass
+
